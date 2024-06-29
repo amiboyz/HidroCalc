e6417bada6cc9ec339fd130944bee2ba8bf86da0
amiboyz <amiboyz@gmail.com>
2024-06-29 22:21:19 +0700

    update

--- HydroCalc_app/HydroCalc_main.py
+++ HydroCalc_app/HydroCalc_main.py
@@ -157,7 +157,7 @@ st.markdown("Masukan informasi pengguna di bawah ini untuk menjalankan kalkulasi
 conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 
 #Fetch existing vendor data
-existing_data = conn.read(worksheet="Data_User_v1")
+existing_data = conn.read(worksheet="Data", usecols=list(range(2)), ttl=5)
 existing_data = existing_data.dropna(how="all")
 
 # st.dataframe(existing_data)
