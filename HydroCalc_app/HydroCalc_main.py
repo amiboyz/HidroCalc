10d5506196194d4de4048ec7a4cd894c07ed6903
amiboyz <amiboyz@gmail.com>
2024-06-30 09:48:09 +0700

    Update HydroCalc_main.py

--- HydroCalc_app/HydroCalc_main.py
+++ HydroCalc_app/HydroCalc_main.py
@@ -519,5 +519,49 @@ if submit_button:
         st.write(Table_T_V_p)
         #st.pyplot(fig2)
         st.write('Tabel Hidrograf Sintetik')
-        st.write(df_Q_T)    
-        #st.pyplot(p)
+        st.write(df_Q_T)
+
+
+        #Fetch existing vendor data
+        existing_data_masukan = conn.read(worksheet="Masukan", usecols=list(range(4)), ttl=5)
+        existing_data_masukan = existing_data_masukan.dropna(how="all")
+
+        # st.dataframe(existing_data)
+        # List of profession
+        
+        # Membuat Form baru
+        with st.form(key="User_Form"):
+            nama_user = nama_user
+            jenis_profesi = jenis_profesi
+            tanggal_akses = datetime.now()
+            jam_akses = datetime.now().time()
+            masukan = st.text_area(label="Mohon Masukan dan Saran pada Aplikasi ini")
+
+            submit_button = st.form_submit_button(label="Kirim Masukan dan Saran")
+
+            # if the submit button is press
+            if submit_button:
+                # Cek if all mandatory field are filled
+                if not nama_user or not jenis_profesi:
+                    st.warning("Pastikan Nama dan Profesi anda terisi")
+                    st.stop()
+                else:
+                    # Create a new row of user
+                    user_data = pd.DataFrame(
+                        [
+                                {
+                                    "Nama_User": nama_user,
+                                    "Profesi": jenis_profesi,
+                                    "Tanggal_Akses": tanggal_akses,
+                                    "Jam_Akses": jam_akses,
+                                    "Masukan": masukan
+                                }
+                        ]
+                    )
+
+                    # Add the new user name to the existing data
+                    update_df = pd.concat([existing_data_masukan, user_data], ignore_index=True)
+
+                    # Update Google Sheets with the user data
+                    conn.update(worksheet="Masukan", data=update_df)
+                    st.success("Pengisian Masukan dan Saran Berhasil")
