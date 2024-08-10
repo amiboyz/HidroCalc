54fa8e3b8a5f49d3d2c7f560723301f00a8fbcf9
amiboyz <amiboyz@gmail.com>
2024-08-10 19:18:56 +0800

    Update HydroCalc_main.py

--- HydroCalc_app/HydroCalc_main.py
+++ HydroCalc_app/HydroCalc_main.py
@@ -357,7 +357,7 @@ if submit_button:
             Q_peak4, t_peak4, V_total4, t_peak_V4,T4,Qtot4,V_cum4 =  calculate_Q_and_V(p,time_step_hours, Q_qp4, time_to_compute)     
             T=T4                                       
         # Membuat p_bar dengan menambahkan nol hingga panjangnya sama dengan T
-        if Metode_infiltrasi == "SCS-CN" and "Horton": 
+        if Metode_infiltrasi == "SCS-CN" or "Horton": 
             p_bar = np.concatenate((p, np.zeros((len(T) - len(p)))))
             Infiltrasi_bar = np.concatenate((Infiltrasi, np.zeros((len(T) - len(Infiltrasi)))))
             plt.figure(figsize=(12, 6))
